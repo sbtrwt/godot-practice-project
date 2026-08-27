79bc17d26847380bfcc676ad0fd2d42b3245f45c
shobhit-rawat <sbtrwt.se@gmail.com>
2026-08-27 22:45:02 +0530

    enemy added

---
+++ project-practice-1/scripts/enemy.gd
@@ -0,0 +1,52 @@
+extends CharacterBody2D
+
+@export var health = 1
+@export var speed = 100.0
+@export var type = "ship"
+
+var destroyed = false
+
+func _ready() -> void:
+	$AnimationPlayerBoom.play("idle")
+	$AnimationPlayerHuting.play("idle")
+
+func _process(delta: float) -> void:
+	if Global.game_on == true and Global.game_over == false:
+		velocity = Vector2(0,1) * speed
+		move_and_slide()
+	if health <= 0 and destroyed == false:
+		destroyed = true
+		Global.score += 1
+		destroy()
+	
+
+func destroy ():
+	speed = 0
+	if type == "ship":
+		$Sprite2D.visible = false
+		$AnimationPlayerBoom.play("hurting")
+		$AudioStreamPlayer2D.play()
+		await $AnimationPlayerBoom.animation_finished
+	queue_free()
+		
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):
+		health -= 3
+		if type == "meteor":
+			area.get_parent().health -= 100
+	if area.is_in_group("laser"):
+		health -= 1
+		if type == "ship" and health > 0 :
+			$AnimationPlayerHuting.play("hurting")
+		if area.get_parent().laser_type != 3:
+			area.get_parent().queue_free()
+		
+		
+	
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
+	#pass
